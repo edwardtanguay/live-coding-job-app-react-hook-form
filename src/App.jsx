b0f954d8c60bea066a6761e911e517998fbbc85f
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-28 10:26:12 +0200

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -146,14 +146,20 @@ function App() {
 						)}
 						<button onClick={handleLogoutButton}>Logout</button>
 					</div>
-					{displayKind === 'full' ? (
+					{displayKind === 'full' && (
 						<JobsFull
 							jobs={jobs}
 							handleStatusChange={handleStatusChange}
 							techItems={techItems}
 						/>
-					) : (
+					)}
+					
+					{displayKind === 'list' && (
 						<JobsList jobs={jobs} />
+					)}
+
+					{displayKind === 'addJob' && (
+					<div>add-job page</div>
 					)}
 				</>
 			) : (
